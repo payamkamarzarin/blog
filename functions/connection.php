61f6ab6cd92ecf3f303d24d58f9dd3cd862b9054
Payam Kamarzarin <payamkamarzarin@yahoo.com>
2024-11-02 16:23:13 +0330

    edit file connection

--- functions/connection.php
+++ functions/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+    $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
     $pdo = new PDO("mysql:host=localhost;port=3308;dbname=blog", "root", "", $option);
     return $pdo;
 //    echo "Connected successfully";
